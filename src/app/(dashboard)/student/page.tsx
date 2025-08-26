b7182f2b1a877935d01bf0b2fceb8f72efbdd728
Hcaglar32 <83988785+Hcaglar32@users.noreply.github.com>
2025-08-26 09:27:24 +0300

    updates

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,6 +1,29 @@
+import dynamic from "next/dynamic";
+
+import Announcements from "@/components/Announcements";
+const EventCalendar = dynamic(() => import("@/components/EventCalendar"), {
+    ssr: false,
+    loading: () => <div className="bg-white p-4 rounded-md h-[300px] flex items-center justify-center">Loading Calendar...</div>
+});
+import 'react-calendar/dist/Calendar.css';
+import BigCalendar from "@/components/BigCalendar";
 const StudentPage = () => {
     return (
-        <div className=''>StudentPage</div>
+        <div className='p-4 flex gap-4 flex-col xl:flex-row'>
+            {/*LEFT*/}
+            <div className="w-full xl:w-2/3">
+            <div className="h-full bg-white p-4 rounded-md">
+                <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+                <BigCalendar/>
+            </div>
+            </div>
+
+            {/*RIGHT*/}
+            <div className="w-full xl:w-1/3 flex flex-col gap-8">
+                <EventCalendar />
+                <Announcements />
+            </div>
+        </div>
     )
 }
 
