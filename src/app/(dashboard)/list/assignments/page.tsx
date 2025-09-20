cf89c9f112a0e4ba8a9b1c9de66be05ab7f4b6ce
Hcaglar32 <83988785+Hcaglar32@users.noreply.github.com>
2025-09-21 01:02:29 +0300

    update

---
+++ src/app/(dashboard)/list/assignments/page.tsx
@@ -0,0 +1,100 @@
+import TableSearch from "@/components/TableSearch";
+import Image from "next/image";
+import Pagination from "@/components/Pagination";
+import Table from "@/components/Table";
+import Link from "next/link";
+import {role, lessonsData, assignmentsData} from "@/lib/data";
+
+type Lesson = {
+    id: number;
+    subject: string;
+    class: string;
+    teacher: string;
+    dueDate: string;
+}
+
+const columns = [
+    {
+        header: "Subject Name", accessor: "name"
+    },
+    {
+        header: "Class",
+        accessor: "class",
+    },
+    {
+        header: "Teacher",
+        accessor: "teacher",
+        className: "hidden md:table-cell"
+    },
+
+
+    {
+        header: "Due Date",
+        accessor: "date",
+        className: "hidden md:table-cell"
+    },
+
+    {
+        header: "Actions", accessor: "action"
+    },
+]
+const AssignmentsListPage = () => {
+    const renderRow = (item: Lesson) => {
+        return (
+            <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight">
+                <td className="flex items-center gap-4 p-4">{item.subject}</td>
+                <td className="">{item.class}</td>
+                <td className="hidden md:table-cell">{item.teacher}</td>
+                <td className="hidden md:table-cell">{item.dueDate}</td>
+                <td>
+                    <div className="flex items-center gap-2">
+                        <Link href={`/list/teachers/${item.id}`}>
+                            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
+                                <Image src="/edit.png" alt="" width={16} height={16}/>
+                            </button>
+                        </Link>
+                        {role === "admin" && (
+                            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
+                                <Image src="/delete.png" alt="" width={16} height={16}/>
+                            </button>
+                        )}
+
+                    </div>
+                </td>
+            </tr>
+        )
+    }
+    return (
+        <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
+            {/*Top*/}
+            <div className="flex items-center justify-between">
+                <h1 className="text-lg font-semibold hidden md:block">All Assigments</h1>
+                <div className="flex flex-col md:flex-row items-center gap-4 w-full  md:w-auto">
+                    <TableSearch/>
+                    <div className="flex items-center gap-4 self-end">
+                        <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+                            <Image src={"/filter.png"} alt="" width={14} height={14}/>
+                        </button>
+                        <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+                            <Image src={"/sort.png"} alt="" width={14} height={14}/>
+                        </button>
+                        {role === "admin" && (
+                            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+                                <Image src={"/plus.png"} alt="" width={14} height={14}/>
+                            </button>)}
+                    </div>
+                </div>
+            </div>
+            {/*List*/}
+            <div className="">
+                <Table columns={columns} renderRow={renderRow} data={assignmentsData}/>
+            </div>
+            {/*Pagination*/}
+
+            <Pagination/>
+
+        </div>
+    )
+}
+
+export default AssignmentsListPage
